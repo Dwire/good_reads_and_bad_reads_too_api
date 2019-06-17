eaf97ae77d43a9660dd56e720b007c602e4d9f87
Dwire <gjdwyer8@gmail.com>
2019-06-16 21:25:55 -0400

    add book review data to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,16 +4,31 @@ class UserSerializer < ActiveModel::Serializer
   # has_many :books,
 
   def read_books
-      object.books.map do |book|
-      {
-        id: book.id,
-        url_link: book.url_link,
-        title: book.title,
-        author: book.author,
-        content: book.content,
-        genre: book.genre
-      }
-    end
+      object.user_books.map do |user_book|
+        {
+          reader_rating: user_book.reader_rating,
+          reader_review: user_book.reader_review,
+          reader_suggestion: user_book.reader_suggestion,
+          book_detail: {
+            id: user_book.book.id,
+            url_link: user_book.book.url_link,
+            title: user_book.book.title,
+            author: user_book.book.author,
+            content: user_book.book.content,
+            genre: user_book.book.genre
+          }
+        }
+      end
+    #   object.books.map do |book|
+    #   {
+    #     id: book.id,
+    #     url_link: book.url_link,
+    #     title: book.title,
+    #     author: book.author,
+    #     content: book.content,
+    #     genre: book.genre
+    #   }
+    # end
   end
 
   def reading_list
